f2643f2b609d608eea52171d1e4a87f62eff00bf
Wiliam Souza <wiliamsouza83@gmail.com>
2018-05-27 00:15:46 -0300

    Change bucket setup test to use start_supervised

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -2,7 +2,7 @@ defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, bucket} = KV.Bucket.start_link([])
+    bucket = start_supervised!(KV.Bucket)
     %{bucket: bucket}
   end
 
