009dabc2101a67b3d7d0cad4daafaf95241a67e1
Wiliam Souza <wiliamsouza83@gmail.com>
2018-05-27 17:39:59 -0300

    Add routing layer and tests

---
+++ test/kv/router_test.exs
@@ -0,0 +1,15 @@
+defmodule KV.RouterTest do
+  use ExUnit.Case, async: true
+
+  @tag :distributed
+  test "route requests across nodes" do
+    assert KV.Router.route("hello", Kernel, :node, []) == :foo@lat
+    assert KV.Router.route("world", Kernel, :node, []) == :bar@lat
+  end
+
+  test "raises on unknown entries" do
+    assert_raise RuntimeError, ~r/could not find entry/, fn ->
+      KV.Router.route(<<0>>, Kernel, :node, [])
+    end
+  end
+end
